60af70ab1e59f0f65d5a67cf51615935491688ac
Przemysław K. Rekucki <prekucki@rcl.pl>
2020-04-06 09:50:23 +0200

    init

---
+++ client/src/market.rs
@@ -0,0 +1,9 @@
+//! Market API part of the Yagna API
+
+mod provider;
+mod requestor;
+
+pub use provider::MarketProviderApi;
+pub use requestor::MarketRequestorApi;
+
+const MARKET_URL_ENV_VAR: &str = "YAGNA_MARKET_URL";
