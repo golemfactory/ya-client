433679fb331c35b45b07cdadd1647dd095a697f9
Piotr Chromiec <tworec@golem.network>
2020-05-14 11:47:20 +0200

    [market client] service url made public

--- src/market.rs
+++ src/market.rs
@@ -8,9 +8,9 @@ pub use requestor::MarketRequestorApi;
 pub(crate) const MARKET_URL_ENV_VAR: &str = "YAGNA_MARKET_URL";
 /// Centralized (Mk1 aka TestBed) Market API instance.
 // TODO: remove it after implementing P2P Market
-const DEFAULT_MARKET_URL: &str = "http://34.244.4.185:8080/market-api/v1/";
+pub const DEFAULT_MARKET_URL: &str = "http://34.244.4.185:8080/market-api/v1/";
 
-fn service_url() -> crate::Result<url::Url> {
+pub fn service_url() -> crate::Result<url::Url> {
     Ok(std::env::var(MARKET_URL_ENV_VAR)
         .unwrap_or(DEFAULT_MARKET_URL.into())
         .parse()?)
