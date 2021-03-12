e0347db6f989bc047a447c639f7b9e5823ed1d49
Paweł Nowosielski <pawel.nowosielski@golem.network>
2021-03-12 14:55:41 +0100

    chore: make formatter happy

--- model/src/payment/driver_details.rs
+++ model/src/payment/driver_details.rs
@@ -1,6 +1,6 @@
 use crate::payment::network::Network;
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct DriverDetails {
