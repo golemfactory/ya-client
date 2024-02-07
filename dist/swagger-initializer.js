9ca55c1df1f68e17146370651955a64bf846edf4
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2024-02-07 18:33:51 +0100

    Add swagger to display documentation

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    urls: [
+      {
+        name: "Market API",
+        url: "../specs/market-api.yaml",
+      },
+      {
+        name: "Activity API",
+        url: "../specs/activity-api.yaml",
+      },
+      {
+        name: "Payment API",
+        url: "../specs/payment-api.yaml",
+      },
+      {
+        name: "NET API",
+        url: "../specs/net-api.yaml",
+      },
+      {
+        name: "NET API v2",
+        url: "../specs/net-api-v2.yaml",
+      },
+      {
+        name: "GSB API",
+        url: "../specs/gsb-api.yaml",
+      },
+    ],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
