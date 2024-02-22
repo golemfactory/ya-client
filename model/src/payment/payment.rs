4f78ccb1c403de33ddc85db99306cf53749f813e
Staszek Krotki <16387248+staszek-krotki@users.noreply.github.com>
2024-02-22 18:56:49 +0100

    signed_bytes

--- model/src/payment/payment.rs
+++ model/src/payment/payment.rs
@@ -10,9 +10,9 @@ pub struct Signed<T> {
     #[serde(flatten)]
     pub payload: T,
     #[serde(with = "serde_bytes")]
-    pub signature: Vec<u8>,
+    pub signature: Option<Vec<u8>>,
     #[serde(with = "serde_bytes")]
-    pub signed_bytes: Vec<u8>
+    pub signed_bytes: Option<Vec<u8>>
 }
 
 
