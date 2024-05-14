bce8be05f43719ba3c6335b8e7bcd116ddd9938f
Przemysław Krzysztof Rekucki <przemyslaw.rekucki@golem.network>
2024-05-14 09:04:35 +0200

    added spec for payment managmet improvements

--- model/src/payment/allocation.rs
+++ model/src/payment/allocation.rs
@@ -1,6 +1,8 @@
+use std::time::Duration;
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use serde_with::*;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Deposit {
@@ -9,6 +11,8 @@ pub struct Deposit {
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde_as]
+#[skip_serializing_none]
 #[serde(rename_all = "camelCase")]
 pub struct Allocation {
     pub allocation_id: String,
@@ -18,11 +22,12 @@ pub struct Allocation {
     pub spent_amount: BigDecimal,
     pub remaining_amount: BigDecimal,
     pub timestamp: DateTime<Utc>,
-    #[serde(skip_serializing_if = "Option::is_none", default)]
     pub timeout: Option<DateTime<Utc>>,
-    #[serde(skip_serializing_if = "Option::is_none", default)]
     pub deposit: Option<Deposit>,
+    #[serde(default)]
     pub make_deposit: bool,
+    #[serde_as(as = "Option<serde_with::DurationSeconds<i64>>")]
+    pub extend_timeout : Option<Duration>
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -40,17 +45,20 @@ pub enum PaymentPlatformEnum {
     PaymentPlatform(PaymentPlatform),
 }
 
+
+#[serde_as]
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct NewAllocation {
     pub address: Option<String>,
     pub payment_platform: Option<PaymentPlatformEnum>,
     pub total_amount: BigDecimal,
-    #[serde(skip_serializing_if = "Option::is_none", default)]
     pub timeout: Option<DateTime<Utc>>,
-    #[serde(skip_serializing_if = "Option::is_none", default)]
     pub deposit: Option<Deposit>,
+    #[serde(default)]
     pub make_deposit: bool,
+    #[serde_as(as = "Option<serde_with::DurationSeconds<u64>>")]
+    pub extend_timeout : Option<Duration>
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -60,4 +68,35 @@ pub struct AllocationUpdate {
     pub total_amount: Option<BigDecimal>,
     #[serde(skip_serializing_if = "Option::is_none", default)]
     pub timeout: Option<DateTime<Utc>>,
+}
+
+#[cfg(test)]
+mod test {
+    use std::fmt::Debug;
+    use serde::de::DeserializeOwned;
+    use super::*;
+
+    fn can_parse_to<T : DeserializeOwned + Debug>(json : &str) {
+        let v : T = serde_json::from_str(json).unwrap();
+        eprintln!("{:?}", v);
+    }
+
+    #[test]
+    fn test_new_allocation() {
+        can_parse_to::<NewAllocation>(r#"{
+            "paymentPlatform": "erc20-polygon-glm",
+            "totalAmount": 1.0,
+            "timeout": "2023-08-28T15:16:31.858Z",
+            "makeDeposit": false,
+            "extendTimeout": 3600
+        }"#);
+        can_parse_to::<NewAllocation>(r#"{
+            "totalAmount": 5
+        }"#);
+        can_parse_to::<NewAllocation>(r#"{
+            "paymentPlatform": { "token": "GLM" },
+            "totalAmount": "512.2345"
+        }"#);
+    }
+
 }
