b32fe465c562daca855db1f34a5f296142a40c20
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2025-04-29 17:17:01 +0200

    Add expiration field to Offer/Demand client model

--- model/src/market/offer.rs
+++ model/src/market/offer.rs
@@ -47,7 +47,8 @@ pub struct Offer {
     pub offer_id: String,
     #[serde(rename = "providerId")]
     pub provider_id: NodeId,
-    // TODO: introduce TTL aka validTo
+    #[serde(rename = "expiration")]
+    pub expiration: DateTime<Utc>,
     /// Object creation timestamp
     #[serde(rename = "timestamp")]
     pub timestamp: DateTime<Utc>,
@@ -59,6 +60,7 @@ impl Offer {
         constraints: String,
         offer_id: String,
         provider_id: NodeId,
+        expiration: DateTime<Utc>,
         timestamp: DateTime<Utc>,
     ) -> Offer {
         Offer {
@@ -66,6 +68,7 @@ impl Offer {
             constraints,
             offer_id,
             provider_id,
+            expiration,
             timestamp,
         }
     }
